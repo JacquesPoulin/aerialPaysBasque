350f307b04149d6c31a9763391ef692f88df6003
Jacques <jacques.poulin64@gmail.com>
2024-08-12 11:35:24 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+	return (
+		<>
+			<h1 className='read-the-docs'>AERIAL CIRCUS</h1>
+		</>
+	);
+}
+
+export default App;
