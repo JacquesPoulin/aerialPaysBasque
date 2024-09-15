3cdbd996c9bf4e75584f99f2a5dad2eea9fd7a60
Jacques <jacques.poulin64@gmail.com>
2024-09-15 20:42:19 +0200

    Site version en prod

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -80,7 +80,7 @@ const Homepage = () => {
                 data-aos="zoom-out-up"
               >
                 <img
-                  src="/assets/pics/cerceau/nature/moi.jpg"
+                  src="/assets/pics/autres/moi.jpg"
                   alt="Laureen Davant"
                   className="w-full h-[45rem] md:w-1/2 rounded-lg object-cover"
                 />
