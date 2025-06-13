63a1e36289f192f79c99c898913644c5d89fabbc
JPO <j.poulin@codeactive.fr>
2025-06-13 12:14:53 +0200

    Suppression du fit&strong

--- src/data/imagesNavList.js
+++ src/data/imagesNavList.js
@@ -2,7 +2,7 @@ const imagesNavList = [
   { id: "cerceau", label: "Cerceau" },
   { id: "pilates", label: "Pilates" },
   { id: "yoga", label: "Yoga" },
-  { id: "Fitness", label: "Fit & Strong" },
+  // { id: "Fitness", label: "Fit & Strong" },
   // { id: "tissu", label: "Tissu" },
   // { id: "infos", label: "Infos" },
 ];
